cb0de09d9491472f9313a99ac69d6aa0ad19f61f
Giacomo212 <giacomo9812@gmail.com>
2020-11-09 11:07:17 +0100

    added database

--- app/src/main/java/pl/damiankoski/androidblogapp/MainActivity.java
+++ app/src/main/java/pl/damiankoski/androidblogapp/MainActivity.java
@@ -3,24 +3,38 @@ package pl.damiankoski.androidblogapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+
+import java.io.Console;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     String titles[];
     String authors[];
-
+    Database database;
     RecyclerView postsList;
 
      @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+         SQLiteDatabase db = this.openOrCreateDatabase("blogApp.db",MODE_PRIVATE,null);
+         database = new Database(db);
+       // setContentView(R.layout.activity_main);
+         
+        try {
+            ArrayList<Post> arrayList = database.QueryPosts();
+            arrayList.size();
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        
+        //titles = getResources().getStringArray(R.array.post_titles);
+        //titles = getResources().getStringArray(R.array.post_authors);
 
-        titles = getResources().getStringArray(R.array.post_titles);
-        titles = getResources().getStringArray(R.array.post_authors);
+        //postsList = findViewById(R.id.posts_list);
 
-        postsList = findViewById(R.id.posts_list);
-
-        PostsAdapter postsAdapter = new PostsAdapter(this, titles, authors);
+        //PostsAdapter postsAdapter = new PostsAdapter(this, titles, authors);
      }
 }
